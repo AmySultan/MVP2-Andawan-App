88befbe3f64cede08f3dcbf2f660ddd6f7b5d495
AmySultan <86156296+AmySultan@users.noreply.github.com>
2021-09-04 06:23:02 +0800

    mvp2 1st commit

--- client/src/Components/Information.js
+++ client/src/Components/Information.js
@@ -5,28 +5,13 @@ function Information() {
 	return (
 		<div>
 			<div className="wrapper">
-    		 {/* Navigation */}
-    			{/* <nav className="main-nav">
-      			<ul>
-        			<li>
-          				<a href="#">Video of NCR cases</a>
-        			</li>
-        			<li>
-          				<a href="#">Latest News</a>
-        			</li>
-        			<li>
-          				<a href="#">On-going NCR cases</a>
-        			</li>
-        			<li>
-          				<a href="#">Native Rights on NCR lands</a>
-        			</li>
-      			</ul>
-    		</nav> */}
+    		 
      		{/* Top Container  */}
     			<section className="top-container">
       				<header className="showcase">
         				<h1>Andawan</h1>
-        				<p>This platform is to educate and inform natives and society on native customary law and update ongoing court cases involving native land.</p>
+        				<p>This platform is to educate and inform natives and society 	 on native customary law and update ongoing court cases involving native land.
+						This is also a platform that connect lawyers/experts to natives that needed legal advise on their land rights. Dedicated lawyers/experts can contribute by sharing their knowledge and give back to community and enhance their reputation.</p>
        					<a href="#" className="btn">Read More</a>
       				</header>
       			</section>
@@ -35,29 +20,37 @@ function Information() {
     		{/* Boxes Section */}
     			<section className="boxes">
       			<div className="box">
-        			<i className="image1"></i>
-        			<h3>Sabah</h3>
-        			<p>Our Land, Our Lives - Claiming Native Title in Sabah, Malaysia (Tanah Kami Kehidupan Kami)</p>
+        			<i className="video-box"></i>
+        			<h4>Video Related to NCR</h4>
+        			<p>Claiming Native Title in Sabah</p>
 					<span>
 					<iframe width="350" height="250" src="https://www.youtube.com/embed/PRhOEdDlvYM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          			Video
-        			</span>
+          			</span>
       			</div>
       			<div className="box">
         			<i className="image2"></i>
-        			<h3>News</h3>
-        			<p>Latest News about NCR in Sabah</p>
+        			<h4>NGO's</h4>
+        			<p>Pacos Trust</p>
+					<span onClick={PacosTrust =>
+	            	(window.location.href ="https://pacostrust.com")}>
+					<iframe width="350" height="250" src="https://pacostrust.com/">
+					</iframe>
+          			</span>
       			</div>
       			<div className="box">
         			<i className="image3"></i>
-        			<h3>Land Ordinance Cap.68</h3>
-        			<p>State Of Sabah's Land Ordinance</p>
+        			<h4>Official Portal of the High Court in Sabah and Sarawak</h4>
+        			<span onClick={Gothere =>
+	            	(window.location.href ="https://judiciary.kehakiman.gov.my/portals/")}>
+					<iframe width="350" height="250" src="https://judiciary.kehakiman.gov.my/portals/"></iframe>
+					Go there
+          			</span>
       			</div>
       			</section>
 
     		{/* Info Section */}
     			<section className="info">
-      				<img src="https://www.borneotoday.net/wp-content/webp-express/webp-images/uploads/2017/02/tana-ncr.jpg.webp" alt="" />
+      				<img src="https://www.borneotoday.net/wp-content/webp-express/webp-images/uploads/2017/02/tana-ncr.jpg.webp" alt="borneotoday" width="500" height="300" />
       			<div>
         			<h2>What's Andawan About</h2>
         			<p>It is a platform for natives in Sabah to connect with lawyers/experts for their traditional land disputes.</p>
